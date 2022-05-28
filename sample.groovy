00e286f54cd1ddae3c080b6ec2abc5df62188658
vath <vath@softwareag.com>
2022-05-28 20:11:55 +0530

    Added Mr

--- sample.groovy
+++ sample.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Hello') {
             steps {
-                echo 'Hello Vasanth'
+                echo 'Hello Mr. Vasanth'
             }
         }
     }
